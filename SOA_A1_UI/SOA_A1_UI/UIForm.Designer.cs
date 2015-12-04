30f34ac6284f6c2beeda0f572aacdec63a3fb2e0
Connor McQuade <ccm519@hotmail.com>
2015-12-03 22:53:23 -0500

    Added logic to the GrabTotal button and the Region list. this includes only allowing one selected region and totaling the price up to send to the service

--- SOA_A1_UI/SOA_A1_UI/UIForm.Designer.cs
+++ SOA_A1_UI/SOA_A1_UI/UIForm.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LoginBtn = new System.Windows.Forms.Button();
-            this.usernamelbl = new System.Windows.Forms.Label();
             this.ItemTotallbl = new System.Windows.Forms.Label();
             this.itemTotal = new System.Windows.Forms.Label();
             this.pstTotal = new System.Windows.Forms.Label();
@@ -40,34 +38,18 @@
             this.GST = new System.Windows.Forms.Label();
             this.totalPrice = new System.Windows.Forms.Label();
             this.TotalPricelbl = new System.Windows.Forms.Label();
-            this.ItemBox = new System.Windows.Forms.ListBox();
             this.ItemPrices = new System.Windows.Forms.ListBox();
             this.GrabTotalBtn = new System.Windows.Forms.Button();
+            this.ItemBox = new System.Windows.Forms.CheckedListBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.regionCheck = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
-            // 
-            // LoginBtn
-            // 
-            this.LoginBtn.Location = new System.Drawing.Point(13, 13);
-            this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(216, 23);
-            this.LoginBtn.TabIndex = 0;
-            this.LoginBtn.Text = "Login";
-            this.LoginBtn.UseVisualStyleBackColor = true;
-            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
-            // 
-            // usernamelbl
-            // 
-            this.usernamelbl.AutoSize = true;
-            this.usernamelbl.Location = new System.Drawing.Point(12, 44);
-            this.usernamelbl.Name = "usernamelbl";
-            this.usernamelbl.Size = new System.Drawing.Size(68, 13);
-            this.usernamelbl.TabIndex = 1;
-            this.usernamelbl.Text = "USERNAME";
             // 
             // ItemTotallbl
             // 
             this.ItemTotallbl.AutoSize = true;
-            this.ItemTotallbl.Location = new System.Drawing.Point(12, 167);
+            this.ItemTotallbl.Location = new System.Drawing.Point(14, 367);
             this.ItemTotallbl.Name = "ItemTotallbl";
             this.ItemTotallbl.Size = new System.Drawing.Size(59, 13);
             this.ItemTotallbl.TabIndex = 2;
@@ -76,7 +58,7 @@
             // itemTotal
             // 
             this.itemTotal.AutoSize = true;
-            this.itemTotal.Location = new System.Drawing.Point(144, 167);
+            this.itemTotal.Location = new System.Drawing.Point(146, 367);
             this.itemTotal.Name = "itemTotal";
             this.itemTotal.Size = new System.Drawing.Size(28, 13);
             this.itemTotal.TabIndex = 3;
@@ -85,7 +67,7 @@
             // pstTotal
             // 
             this.pstTotal.AutoSize = true;
-            this.pstTotal.Location = new System.Drawing.Point(144, 190);
+            this.pstTotal.Location = new System.Drawing.Point(146, 390);
             this.pstTotal.Name = "pstTotal";
             this.pstTotal.Size = new System.Drawing.Size(28, 13);
             this.pstTotal.TabIndex = 5;
@@ -94,7 +76,7 @@
             // PST
             // 
             this.PST.AutoSize = true;
-            this.PST.Location = new System.Drawing.Point(12, 190);
+            this.PST.Location = new System.Drawing.Point(14, 390);
             this.PST.Name = "PST";
             this.PST.Size = new System.Drawing.Size(31, 13);
             this.PST.TabIndex = 4;
@@ -103,7 +85,7 @@
             // hstTotal
             // 
             this.hstTotal.AutoSize = true;
-            this.hstTotal.Location = new System.Drawing.Point(144, 213);
+            this.hstTotal.Location = new System.Drawing.Point(146, 413);
             this.hstTotal.Name = "hstTotal";
             this.hstTotal.Size = new System.Drawing.Size(28, 13);
             this.hstTotal.TabIndex = 7;
@@ -112,7 +94,7 @@
             // HST
             // 
             this.HST.AutoSize = true;
-            this.HST.Location = new System.Drawing.Point(12, 213);
+            this.HST.Location = new System.Drawing.Point(14, 413);
             this.HST.Name = "HST";
             this.HST.Size = new System.Drawing.Size(32, 13);
             this.HST.TabIndex = 6;
@@ -121,7 +103,7 @@
             // gstTotal
             // 
             this.gstTotal.AutoSize = true;
-            this.gstTotal.Location = new System.Drawing.Point(144, 234);
+            this.gstTotal.Location = new System.Drawing.Point(146, 434);
             this.gstTotal.Name = "gstTotal";
             this.gstTotal.Size = new System.Drawing.Size(28, 13);
             this.gstTotal.TabIndex = 9;
@@ -130,7 +112,7 @@
             // GST
             // 
             this.GST.AutoSize = true;
-            this.GST.Location = new System.Drawing.Point(12, 234);
+            this.GST.Location = new System.Drawing.Point(14, 434);
             this.GST.Name = "GST";
             this.GST.Size = new System.Drawing.Size(32, 13);
             this.GST.TabIndex = 8;
@@ -139,7 +121,7 @@
             // totalPrice
             // 
             this.totalPrice.AutoSize = true;
-            this.totalPrice.Location = new System.Drawing.Point(144, 256);
+            this.totalPrice.Location = new System.Drawing.Point(146, 456);
             this.totalPrice.Name = "totalPrice";
             this.totalPrice.Size = new System.Drawing.Size(28, 13);
             this.totalPrice.TabIndex = 11;
@@ -148,46 +130,100 @@
             // TotalPricelbl
             // 
             this.TotalPricelbl.AutoSize = true;
-            this.TotalPricelbl.Location = new System.Drawing.Point(12, 256);
+            this.TotalPricelbl.Location = new System.Drawing.Point(14, 456);
             this.TotalPricelbl.Name = "TotalPricelbl";
             this.TotalPricelbl.Size = new System.Drawing.Size(58, 13);
             this.TotalPricelbl.TabIndex = 10;
             this.TotalPricelbl.Text = "Total Price";
             // 
-            // ItemBox
-            // 
-            this.ItemBox.FormattingEnabled = true;
-            this.ItemBox.Location = new System.Drawing.Point(13, 60);
-            this.ItemBox.Name = "ItemBox";
-            this.ItemBox.Size = new System.Drawing.Size(120, 95);
-            this.ItemBox.TabIndex = 12;
-            // 
             // ItemPrices
             // 
             this.ItemPrices.FormattingEnabled = true;
-            this.ItemPrices.Location = new System.Drawing.Point(139, 60);
+            this.ItemPrices.Items.AddRange(new object[] {
+            "8.25",
+            "4.50",
+            "2.99",
+            "10.99"});
+            this.ItemPrices.Location = new System.Drawing.Point(138, 59);
             this.ItemPrices.Name = "ItemPrices";
-            this.ItemPrices.Size = new System.Drawing.Size(90, 95);
+            this.ItemPrices.Size = new System.Drawing.Size(90, 264);
             this.ItemPrices.TabIndex = 13;
             // 
             // GrabTotalBtn
             // 
-            this.GrabTotalBtn.Enabled = false;
-            this.GrabTotalBtn.Location = new System.Drawing.Point(12, 277);
+            this.GrabTotalBtn.Location = new System.Drawing.Point(13, 341);
             this.GrabTotalBtn.Name = "GrabTotalBtn";
-            this.GrabTotalBtn.Size = new System.Drawing.Size(217, 23);
+            this.GrabTotalBtn.Size = new System.Drawing.Size(161, 23);
             this.GrabTotalBtn.TabIndex = 14;
             this.GrabTotalBtn.Text = "Grab Total";
             this.GrabTotalBtn.UseVisualStyleBackColor = true;
+            this.GrabTotalBtn.Click += new System.EventHandler(this.GrabTotalBtn_Click);
+            // 
+            // ItemBox
+            // 
+            this.ItemBox.FormattingEnabled = true;
+            this.ItemBox.Items.AddRange(new object[] {
+            "GIORP",
+            "ITEM 2",
+            "ITEM 3",
+            "ITEM 4"});
+            this.ItemBox.Location = new System.Drawing.Point(11, 60);
+            this.ItemBox.Name = "ItemBox";
+            this.ItemBox.Size = new System.Drawing.Size(118, 259);
+            this.ItemBox.TabIndex = 15;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(13, 13);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(348, 23);
+            this.LoginBtn.TabIndex = 0;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.Location = new System.Drawing.Point(180, 341);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(161, 23);
+            this.clearbtn.TabIndex = 17;
+            this.clearbtn.Text = "Clear All";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // regionCheck
+            // 
+            this.regionCheck.FormattingEnabled = true;
+            this.regionCheck.Items.AddRange(new object[] {
+            "NL",
+            "NS",
+            "NB",
+            "PE",
+            "QC",
+            "ON",
+            "MB",
+            "SK",
+            "AB",
+            "BC",
+            "YT",
+            "NT",
+            "NU"});
+            this.regionCheck.Location = new System.Drawing.Point(243, 59);
+            this.regionCheck.Name = "regionCheck";
+            this.regionCheck.Size = new System.Drawing.Size(118, 259);
+            this.regionCheck.TabIndex = 18;
+            this.regionCheck.SelectedIndexChanged += new System.EventHandler(this.regionCheck_SelectedIndexChanged);
             // 
             // UIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(242, 312);
+            this.ClientSize = new System.Drawing.Size(373, 476);
+            this.Controls.Add(this.regionCheck);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.ItemBox);
             this.Controls.Add(this.GrabTotalBtn);
             this.Controls.Add(this.ItemPrices);
-            this.Controls.Add(this.ItemBox);
             this.Controls.Add(this.totalPrice);
             this.Controls.Add(this.TotalPricelbl);
             this.Controls.Add(this.gstTotal);
@@ -198,7 +234,6 @@
             this.Controls.Add(this.PST);
             this.Controls.Add(this.itemTotal);
             this.Controls.Add(this.ItemTotallbl);
-            this.Controls.Add(this.usernamelbl);
             this.Controls.Add(this.LoginBtn);
             this.Name = "UIForm";
             this.Text = "Form1";
@@ -208,9 +243,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button LoginBtn;
-        private System.Windows.Forms.Label usernamelbl;
         private System.Windows.Forms.Label ItemTotallbl;
         private System.Windows.Forms.Label itemTotal;
         private System.Windows.Forms.Label pstTotal;
@@ -221,9 +253,12 @@
         private System.Windows.Forms.Label GST;
         private System.Windows.Forms.Label totalPrice;
         private System.Windows.Forms.Label TotalPricelbl;
-        private System.Windows.Forms.ListBox ItemBox;
         private System.Windows.Forms.ListBox ItemPrices;
         private System.Windows.Forms.Button GrabTotalBtn;
+        private System.Windows.Forms.CheckedListBox ItemBox;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Button clearbtn;
+        private System.Windows.Forms.CheckedListBox regionCheck;
     }
 }
 
--- SOA_A1_UI/SOA_A1_UI/UIForm.cs
+++ SOA_A1_UI/SOA_A1_UI/UIForm.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Web;
 namespace SOA_A1_UI
 {
     public partial class UIForm : Form
@@ -23,15 +23,40 @@ namespace SOA_A1_UI
 
             //insert those details into the
 
-            usernamelbl.Text = "EXAMPLE";//CUSTOMER NAME HERE
+           // usernamelbl.Text = "EXAMPLE";//CUSTOMER NAME HERE
 
             for (int i = 0; i < numberOfPurchases; i++)
             {
-                ItemBox.Items.Add("ItemAt["+ i +"]");//ITEM BEING PURCHASED HERE
-                ItemBox.Items.Add("ItemPriceAt[" + i + "]");//PRICE OF ITEM BEING PURCHASED HERE
+                //ItemB.Items.Add("ItemAt["+ i +"]");//ITEM BEING PURCHASED HERE
+                //ItemB.Items.Add("ItemPriceAt[" + i + "]");//PRICE OF ITEM BEING PURCHASED HERE
                 totalItemPrice += 1.25;//ITEMS PRICE BEING ADDED UP FOR TOTAL
             }
             itemTotal.Text = totalItemPrice.ToString();
+
+        }
+
+        private void GrabTotalBtn_Click(object sender, EventArgs e)
+        {
+            double totalItemPrice = 0;
+            for (int i = 0; i < ItemBox.Items.Count; i++)
+            {
+                //check to see if the current index is checked
+                ItemBox.GetItemChecked(i);
+
+                //add the price of the checked item to the item total
+                totalItemPrice += Convert.ToDouble(ItemPrices.Items.IndexOf(i).ToString());
+            }
+
+            //display the users total purchase (might change this to an onclick event)
+            itemTotal.Text = totalItemPrice.ToString();
+        }
+
+        private void regionCheck_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (int i in regionCheck.CheckedIndices)
+            {
+                regionCheck.SetItemCheckState(i, CheckState.Unchecked);
+            }
         }
     }
 }
