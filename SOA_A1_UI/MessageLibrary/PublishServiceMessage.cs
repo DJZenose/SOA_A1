06f8e7e79542d2336d4d47d2648e75a9c9225a6d
BrandonErb <brandonerb@gmail.com>
2015-12-07 18:55:57 -0500

    Publish Message Done

--- SOA_A1_UI/MessageLibrary/PublishServiceMessage.cs
+++ SOA_A1_UI/MessageLibrary/PublishServiceMessage.cs
@@ -18,18 +18,29 @@ namespace MessageLibrary
 
             IFormatter formatter = new BinaryFormatter();
             Data data = (Data)formatter.Deserialize(serializedClass);
+            message = "DRC|PUB-SERVICE|<" + data.teamName + ">|<" + data.teamID + ">|" + "\n" +
+                    "SRV|<" + data.serviceTag + ">|<" + data.serviceName + ">|<" + data.securityLevel + ">|<" + data.numArg + ">|<" + data.numResp + ">|<" + data.description + ">|" + "\n" +
+                    "ARG|<" + data.argPosition[0] + ">|<" + data.argName[0] + ">|<" + data.argDataType[0] + ">|[mandatory|optional]||" + "\n" +
+                    "ARG|<" + data.argPosition[1] + ">|<" + data.argName[1] + ">|<" + data.argDataType[1] + ">|[mandatory|optional]||" + "\n" +
+                    "RSP|<" + data.responsePos[0] + ">|<" + data.responseName[0] + ">|<" + data.respDataType[0] + ">||" + "\n" +
+                    "RSP|<" + data.responsePos[1] + ">|<" + data.responseName[1] + ">|<" + data.respDataType[1] + ">||" + "\n" +
+                    "RSP|<" + data.responsePos[2] + ">|<" + data.responseName[2] + ">|<" + data.respDataType[2] + ">||" + "\n" +
+                    "RSP|<" + data.responsePos[3] + ">|<" + data.responseName[3] + ">|<" + data.respDataType[3] + ">||" + "\n" +
+                    "RSP|<" + data.responsePos[4] + ">|<" + data.responseName[4] + ">|<" + data.respDataType[4] + ">||" + "\n" +
+                    "MCH|<" + data.publishIP + ">|<" + data.publishPort + ">|";
 
-            message = "DRC|PUB-SERVICE|<" + data + ">|<" + teamID + ">|" + "\n" +
-                    "SRV|<" + tagName + ">|<" + serviceName + ">|<" + securityLevel + ">|<" + numArgs + ">|<" + numResponses + ">|<" + description + ">|" + "\n" +
-                    "ARG|<" + argPos + ">|<" + argName + ">|<" + argType + ">|[mandatory|optional]||" + "\n" +
-                    "ARG|<" + argPos + ">|<" + argName + ">|<" + argType + ">|[mandatory|optional]||" + "\n" +
-                    "RSP|<" + respPos + ">|<" + respName + ">|<" + respType + ">||" + "\n" +
-                    "RSP|<" + respPos + ">|<" + respName + ">|<" + respType + ">||" + "\n" +
-                    "RSP|<" + respPos + ">|<" + respName + ">|<" + respType + ">||" + "\n" +
-                    "RSP|<" + respPos + ">|<" + respName + ">|<" + respType + ">||" + "\n" +
-                    "RSP|<" + respPos + ">|<" + respName + ">|<" + respType + ">||" + "\n" +
-                    "MCH|<" + psIP + ">|<" + pPort + ">|";
-
+            /*
+            message = "DRC|PUB-SERVICE|<" + data.teamName + ">|<" + data.teamID + ">|" + "\n" +
+                    "SRV|<" + data.tagName + ">|<" + data.serviceName + ">|<" + data.securityLevel + ">|<" + data.numArgs + ">|<" + data.numResponses + ">|<" + data.description + ">|" + "\n" +
+                    "ARG|<" + data.argPos + ">|<" + data.argName + ">|<" + data.argType + ">|[mandatory|optional]||" + "\n" +
+                    "ARG|<" + data.argPos + ">|<" + data.argName + ">|<" + data.argType + ">|[mandatory|optional]||" + "\n" +
+                    "RSP|<" + data.respPos + ">|<" + data.respName + ">|<" + data.respType + ">||" + "\n" +
+                    "RSP|<" + data.respPos + ">|<" + data.respName + ">|<" + data.respType + ">||" + "\n" +
+                    "RSP|<" + data.respPos + ">|<" + data.respName + ">|<" + data.respType + ">||" + "\n" +
+                    "RSP|<" + data.respPos + ">|<" + data.respName + ">|<" + data.respType + ">||" + "\n" +
+                    "RSP|<" + data.respPos + ">|<" + data.respName + ">|<" + data.respType + ">||" + "\n" +
+                    "MCH|<" + data.psIP + ">|<" + data.pPort + ">|";
+            */
             return message;
         }
 
