3b5456896aa1a94ef86a76df0dbdcc6fc4e81a8e
BrandonErb <brandonerb@gmail.com>
2015-12-07 18:32:50 -0500

    Added arg and ret values.

--- SOA_A1_UI/DataClass/Data.cs
+++ SOA_A1_UI/DataClass/Data.cs
@@ -21,9 +21,12 @@ namespace DataClass
         public int[] argPosition = new int[2];
         public string[] argName = new string[2];
         public string[] argDataType = new string[2];
+        public double argValue1;
+        public string argValue2;
         public int[] responsePos = new int [5];
         public string[] responseName = new string[5];
-        public string[] resDataType = new string[5];
+        public string[] respDataType = new string[5];
+        public double[] respValue = new double[5];
         public string publishIP;
         public int publishPort;
 
