e5823fd1500fdb8ffef9d477f9c6ea1bf02c7ed3
DJZenose <ccm519@hotmail.com>
2015-12-08 00:19:23 -0500

    Changed ui to .net4.6

--- SOA_A1_UI/SOA_A1_UI/UIForm.cs
+++ SOA_A1_UI/SOA_A1_UI/UIForm.cs
@@ -66,7 +66,11 @@ namespace SOA_A1_UI
         private void Call_PurchaseTotaller(double totalItemPrice, string regionCode)
         {
 
-
+            cmdConnect();
+            cmdSendData();
+            cmdReceiveData();
+            cmdClose();
+                /*
             try
             {
                 //check to see that the itemprice is valid & the postal code is valid
@@ -85,7 +89,7 @@ namespace SOA_A1_UI
             {
                 //use logerror to send the error message to the log file
                 logError(ex.Message);
-            }
+            }*/
         }
         private void regionCheck_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -267,32 +271,6 @@ namespace SOA_A1_UI
             // Connect to a remote device.
             try
             {
-                // Establish the remote endpoint for the socket.
-                // The name of the 
-                // remote device is "host.contoso.com".
-                IPHostEntry ipHostInfo = Dns.Resolve("192.156.111.70");
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-
-                // Create a TCP/IP socket.
-                Socket client = new Socket(AddressFamily.InterNetwork,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect to the remote endpoint.
-                client.BeginConnect(remoteEP,
-                    new AsyncCallback(ConnectCallback), client);
-                connectDone.WaitOne();
-
-                // Send test data to the remote device.
-                Send(client, messageToSend);
-                sendDone.WaitOne();
-
-                // Receive the response from the remote device.
-                Receive(client);
-                receiveDone.WaitOne();
-
-                // Write the response to the console.
-                Console.WriteLine("Response received : {0}", response);
                 
                 IFormatter formatter = new BinaryFormatter();
                 responseStream = new System.IO.MemoryStream();
@@ -323,107 +301,11 @@ namespace SOA_A1_UI
                             break;
                     }
                 }
-                
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-                //close the stream for the response from the server so we do not consume too many resources
-                responseStream.Close();
-                // Release the socket.
-                
-
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-        }
-
-        private static void ConnectCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete the connection.
-                client.EndConnect(ar);
-
-                Console.WriteLine("Socket connected to {0}",
-                    client.RemoteEndPoint.ToString());
-
-                // Signal that the connection has been made.
-                connectDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        private static void Receive(Socket client)
-        {
-            try
-            {
-                // Create the state object.
-                StateObject state = new StateObject();
-                state.workSocket = client;
-
-                // Begin receiving the data from the remote device.
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(ReceiveCallback), state);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        private static void ReceiveCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the state object and the client socket 
-                // from the asynchronous state object.
-                StateObject state = (StateObject)ar.AsyncState;
-                Socket client = state.workSocket;
-
-                // Read data from the remote device.
-                int bytesRead = client.EndReceive(ar);
-
-                if (bytesRead > 0)
-                {
-                    // There might be more data, so store the data received so far.
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-
-                    // Get the rest of the data.
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                        new AsyncCallback(ReceiveCallback), state);
-                }
-                else
-                {
-                    // All the data has arrived; put it in response.
-                    if (state.sb.Length > 1)
-                    {
-                        response = state.sb.ToString();
-                    }
-                    // Signal that all bytes have been received.
-                    receiveDone.Set();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        private static void Send(Socket client, String data)
-        {
-            // Convert the string data to byte data using ASCII encoding.
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data to the remote device.
-            client.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), client);
         }
 
         private void Registerbtn_Click(object sender, EventArgs e)
@@ -436,24 +318,63 @@ namespace SOA_A1_UI
             UnregisterTeam();
         }
 
-        private static void SendCallback(IAsyncResult ar)
+        private void cmdConnect()
         {
             try
             {
-                // Retrieve the socket from the state object.
-                Socket client = (Socket)ar.AsyncState;
+                //create a new client socket ...
+                m_socWorker = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                String szIPSelected = "192.156.111.70";
+                String szPort = "5555";
+                int alPort = System.Convert.ToInt16(szPort, 10);
 
-                // Complete sending the data to the remote device.
-                int bytesSent = client.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
-
-                // Signal that all bytes have been sent.
-                sendDone.Set();
+                System.Net.IPAddress remoteIPAddress = System.Net.IPAddress.Parse(szIPSelected);
+                System.Net.IPEndPoint remoteEndPoint = new System.Net.IPEndPoint(remoteIPAddress, alPort);
+                m_socWorker.Connect(remoteEndPoint);
             }
-            catch (Exception e)
+            catch (System.Net.Sockets.SocketException se)
             {
-                Console.WriteLine(e.ToString());
+                MessageBox.Show(se.Message);
             }
         }
+
+        private void cmdSendData()
+        {
+            try
+            {
+                //Data objData = IDValidLabel.Text;
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes("WOAH DID IT WORK?");
+                m_socWorker.Send(byData);
+            }
+            catch (System.Net.Sockets.SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        private void cmdReceiveData()
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int iRx = m_socWorker.Receive(buffer);
+                char[] chars = new char[iRx];
+
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(buffer, 0, iRx, chars, 0);
+                System.String szData = new System.String(chars);
+                IDValidLabel.Text = szData;
+            }
+            catch (System.Net.Sockets.SocketException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+        }
+
+        private void cmdClose()
+        {
+            m_socWorker.Close();
+        }
+
     }
 }
