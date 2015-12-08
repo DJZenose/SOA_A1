5b7f720eb665bb99c41d23073e7d21b11b8a937d
DallasThibodeau <Dthibodeau-cc@conestogac.on.ca>
2015-12-08 02:04:11 -0500

    Adding registryConnector

---
+++ SOA_A1_UI/MessageLibrary/registryConnector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLibrary
+{
+    class registryConnector
+    {
+        private static string connectReg(string messageToSend, string registerIP)
+        {
+            Int32 port = 3128;
+            TcpClient regSock = new TcpClient(registerIP, port);
+            NetworkStream regStream;
+
+            try
+            {
+                regSock.Connect(registerIP, port);
+                regStream = regSock.GetStream();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            byte[] register = System.Text.Encoding.ASCII.GetBytes(messageToSend);
+
+
+            regStream.Write(register, 0, register.Length);
+            regStream.Flush();
+
+            //get response
+            byte[] responseStream = new byte[80025];
+            regStream.Read(responseStream, 0, regSock.ReceiveBufferSize);
+            string responseStr = System.Text.Encoding.ASCII.GetString(responseStream);
+
+            regStream.Close();
+            regSock.Close();
+
+            return responseStr;
+        }
+    }
+}
