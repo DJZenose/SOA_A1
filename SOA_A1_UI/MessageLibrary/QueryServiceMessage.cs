ea26aa3bd3896595905363695b1e63b6d0d39194
BrandonErb <brandonerb@gmail.com>
2015-12-07 23:36:08 -0500

    Message Library Done.....For now

--- SOA_A1_UI/MessageLibrary/QueryServiceMessage.cs
+++ SOA_A1_UI/MessageLibrary/QueryServiceMessage.cs
@@ -1,21 +1,104 @@
-﻿using System;
+﻿/*************
+*Programmers    : Connor McQuade & Brandon Erb & Dallas Thibodeau
+*Professor      : Ed Barsalou
+*Date           : 6/12/2015
+*FILE           : QueryServiceMessag.cs
+*Description    : Class Library for Messaging
+**************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using DataClass;
 
 namespace MessageLibrary
 {
     public class QueryServiceMessage
     {
-        public string SendQueryServiceMessage(string teamName, string teamID, string tagName)
+        /*
+        * Method        : SendQueryServiceMessagee
+        * Returns       : string of registery query
+        * Parameters    : Stream of the class containing the needed data
+        * Description   : Builds string to send to the registry
+        */
+        public string SendQueryServiceMessage(Stream serialClass)
         {
-            return "DRC|QUERY-SERVICE|<" + teamName + ">|<" + teamID + ">|SRV|<" + tagName + ">||||||";
+            string message;
+            IFormatter formatter = new BinaryFormatter();
+            Data data = (Data)formatter.Deserialize(serialClass);
+
+            message = "DRC|QUERY-SERVICE|<" + data.teamName + ">|<" + data.teamID + ">|\n" +
+                   "SRV|<" + data.serviceTag + ">|" + data.publishPort + "|";
+            return message;
         }
 
-        public string ParseQueryServiceMessage()
+        /*
+        * Method        : SendQueryServiceMessagee
+        * Returns       : string of registery query
+        * Parameters    : Stream of the class containing the needed data
+        * Description   : Builds string to send to the registry
+        */
+        public Stream ParseQueryServiceMessage(string message, Stream serialClass)
         {
-            return "";
+            IFormatter formatter1 = new BinaryFormatter();
+            Data data = (Data)formatter1.Deserialize(serialClass);
+
+            char[] delimiterChars = {'|'};
+            string[] words = message.Split(delimiterChars);
+
+            if (words[1] == "OK")
+            {
+                //data.segments = words[4];
+                data.teamName = words[6];
+                data.serviceName = words[7];
+                data.numArg = Convert.ToInt32(words[9]);
+                data.numResp = Convert.ToInt32(words[10]);
+                data.description = words[11];
+                //arg 1
+                data.argPosition[0] = Convert.ToInt32(words[13]);
+                data.argName[0] = words[14];
+                data.argDataType[0] = words[15];
+                //arg 2
+                data.argPosition[1] = Convert.ToInt32(words[19]);
+                data.argName[1] = words[20];
+                data.argDataType[1] = words[21];
+                //ret 1
+                data.responsePos[0] = Convert.ToInt32(words[25]);
+                data.responseName[0] = words[26];
+                data.respDataType[0] = words[27];
+                //ret 2
+                data.responsePos[1] = Convert.ToInt32(words[30]);
+                data.responseName[1] = words[31];
+                data.respDataType[1] = words[32];
+                //ret 3
+                data.responsePos[2] = Convert.ToInt32(words[35]);
+                data.responseName[2] = words[36];
+                data.respDataType[2] = words[37];
+                //ret 4
+                data.responsePos[3] = Convert.ToInt32(words[40]);
+                data.responseName[3] = words[41];
+                data.respDataType[3] = words[42];
+                //ret 5
+                data.responsePos[4] = Convert.ToInt32(words[45]);
+                data.responseName[4] = words[46];
+                data.respDataType[4] = words[47];
+                //MCH
+                data.publishIP = words[50];
+                data.publishPort = Convert.ToInt32(words[51]);
+            }
+            else
+            {
+                data.message = message;
+            }
+
+            IFormatter formatter2 = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            formatter2.Serialize(stream, data);
+            return stream;
         }
     }
 }
--- SOA_A1_UI/MessageLibrary/QueryTeamMessage.cs
+++ SOA_A1_UI/MessageLibrary/QueryTeamMessage.cs
@@ -1,22 +1,70 @@
-﻿using System;
+﻿/*************
+*Programmers    : Connor McQuade & Brandon Erb & Dallas Thibodeau
+*Professor      : Ed Barsalou
+*Date           : 6/12/2015
+*FILE           : QueryTeamMessage.cs
+*Description    : Class Library for Messaging
+**************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using DataClass;
 
 namespace MessageLibrary
 {
     public class QueryTeamMessage
     {
-        public string SendQueryTeamMessage(string teamName, string teamID, string serviceTagName)
-        {
-            return "DRC|QUERY-TEAM|<" + teamName + ">|<" + teamID + ">|\nINF|<" + teamName + ">|<" + teamID + ">|<" + serviceTagName + ">|";
 
+        /*
+        * Method        : SendQueryTeamMessage
+        * Returns       : string of register query
+        * Parameters    : Stream of the class containing the needed data
+        * Description   : Builds string to send to the registry
+        */
+        public string SendQueryTeamMessage(Stream serialClass)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Data data = (Data)formatter.Deserialize(serialClass);
+
+            string message;
+            message = "DRC|QUERY-TEAM|<" + data.teamName + ">|<" + data.teamID + ">|\n" +
+                    "INF|<" + data.teamName + ">|<" + data.teamID + ">|<" + data.serviceTag + ">|";
+
+            return message;
         }
 
-        public string ParseQueryTeamMessage (string message)
+        /*
+        * Method        : ParseQueryTeamMessage
+        * Returns       : Stream of the class containing the needed data
+        * Parameters    : Stream of the class containing the needed data and a string of the message to be parsed
+        * Description   : Builds string to send to the registry
+        */
+        public Stream ParseQueryTeamMessage (string message, Stream serialClass)
         {
-            return "";
+            IFormatter formatter1 = new BinaryFormatter();
+            Data data = (Data)formatter1.Deserialize(serialClass);
+
+            char[] delimiterChars = { '|', '|', '|' };
+            string[] words = message.Split(delimiterChars);
+
+            if (words[1] == "OK")
+            {
+                data.message = words[1];
+            }
+            else
+            {
+                data.message = message;
+            }
+
+            IFormatter formatter2 = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            formatter2.Serialize(stream, data);
+            return stream;
         }
     }
 }
