60165ef0bdf5782e674b67723f8d54cf718d5d49
DallasThibodeau <Dthibodeau-cc@conestogac.on.ca>
2015-12-08 12:53:33 -0500

    Fixed service, fixed several messagelibrary functions

--- SOA_A1_UI/ConsoleApplication1/Server.cs
+++ SOA_A1_UI/ConsoleApplication1/Server.cs
@@ -3,127 +3,193 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using MessageLibrary;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using DataClass;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Service
 {
     // State object for reading client data asynchronously
-    public class Service
+    public class StateObject
     {
-        public static void Main()
+        // Client  socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 1024;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class AsynchronousSocketListener
+    {
+        // Thread signal.
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public static string registerIP;
+
+        public AsynchronousSocketListener()
         {
-            TcpListener server = null;
+        }
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // The DNS name of the computer
+            // running the listener is "host.contoso.com".
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.
             try
             {
-                Int32 port = 6600;
-                NetworkInterface.GetAllNetworkInterfaces();
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
 
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[1048];
-                string data = null;
-
-                // Enter the listening loop.
                 while (true)
                 {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
 
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
 
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    stream.Read(bytes, 0, bytes.Length);
-                    
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytes.Length);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                        string request = msg.ToString();
-
-
-                    //Format Request
-                    Data service = new Data();
-                    service.serviceTag = "GIORP-TOTALER";
-
-                    char[] delimiterChars = { '|' };
-                    string[] words = request.Split(delimiterChars);
-
-                    service.teamName = words[2];
-                    service.teamID = words[3];
-                    service.serviceName = words[5];
-                    service.numArg = Convert.ToInt32(words[7]);
-                    //arg 1
-                    service.argPosition[0] = Convert.ToInt32(words[12]);
-                    service.argName[0] = words[14];
-                    service.argDataType[0] = words[15];
-                    service.argValue1 = Convert.ToDouble(words[17]);
-                    //arg 2
-                    service.argPosition[1] = Convert.ToInt32(words[19]);
-                    service.argName[1] = words[20];
-                    service.argDataType[1] = words[21];
-                    service.argValue2 = words[23];
-
-                    string message = MessageLibrary.QueryTeamMessage.SendQueryTeamMessage(service);
-                    
-                    string response = MessageLibrary.registryConnector.connectReg(message, "10.113.21.20", 3000);
-                    string ret = "";
-                    if ( response == "OK")
-                    {
-                       double[] results = new double[5];
-                       results = Purchase_Totaller_BL.Totaller.getTotal(service.argValue2, service.argValue1);
-                       msg = Encoding.ASCII.GetBytes(MessageLibrary.ExecuteServiceMessage.createReturnMessage(results));
-                    }
-                    else
-                    {
-                        msg = Encoding.ASCII.GetBytes(ret);
-                    }
-                    // Send back a response.
-                    stream.Write(msg, 0, msg.Length);
-                    // Shutdown and end connection
-                    client.Close();
-
-
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
                 }
+
             }
-            catch (SocketException e)
+            catch (Exception e)
             {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
+                Console.WriteLine(e.ToString());
             }
 
-
-            Console.WriteLine("\nHit enter to continue...");
+            Console.WriteLine("\nPress ENTER to continue...");
             Console.Read();
         }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.
+            allDone.Set();
+
+            // Get the socket that handles the client request.
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+            char[] finalDelCA = new char[] { (char)13, (char)28, (char)13 };
+            string finalDel = new string(finalDelCA);
+
+            // Retrieve the state object and the handler socket
+            // from the asynchronous state object.
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket. 
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read 
+                // more data.
+                content = state.sb.ToString();
+                if (content.IndexOf(finalDel) > -1)
+                {
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    messageRegister(handler, content);
+                }
+                else
+                {
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Not all data received. Get more.
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void messageRegister(Socket handler, String data)
+        {
+            Data dataFromClient = ExecuteServiceMessage.ParseExecuteServiceMessage(data);
+            string message = QueryTeamMessage.SendQueryTeamMessage(dataFromClient);
+            string answer = registryConnector.connectReg(message, registerIP, 3128);
+            string isOkay = QueryTeamMessage.ParseQueryTeamMessage(answer);
+
+            double[] returnedData = Purchase_Totaller_BL.Totaller.getTotal(dataFromClient.argValue2, dataFromClient.argValue1);
+
+            string messageToClient = ExecuteServiceMessage.createReturnMessage(returnedData);
+
+            SendToClient(handler, messageToClient);
+        }
+
+        private static void SendToClient(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        public static int Main(String[] args)
+        {
+            Console.WriteLine("Please input register IP: ");
+            registerIP = Console.ReadLine();
+            StartListening();
+            return 0;
+        }
     }
- 
 }
--- SOA_A1_UI/MessageLibrary/ExecuteServiceMessage.cs
+++ SOA_A1_UI/MessageLibrary/ExecuteServiceMessage.cs
@@ -50,21 +50,39 @@ namespace MessageLibrary
         * Parameters    : string of the registry reply
         * Description   : parses registry response
         */
-        public static Stream ParseExecuteServiceMessage(string dataToParse)
+        public static Data ParseExecuteServiceMessage(string dataToParse)
         {
             Data dataParsed = new Data();
+            char[] delimiters = new char[] { '|' };
+            double temp;
+            string[] dataUnassigned = dataToParse.Split(delimiters);
 
-            char[] delimiterChars = { '|' };
-            string[] words = dataToParse.Split(delimiterChars);
+            dataParsed.teamName = dataUnassigned[2];
+            dataParsed.teamID = dataUnassigned[3];
+            dataParsed.serviceName = dataUnassigned[5];
+            dataParsed.numArg = Convert.ToInt32(dataUnassigned[6]);
 
-            dataParsed.numSegments = Convert.ToInt32(words[4]);
+            if (double.TryParse(dataUnassigned[11], out temp) == true)
+            {
+                dataParsed.argPosition[0] = Convert.ToInt32(dataUnassigned[8]);
+                dataParsed.argName[0] = dataUnassigned[9];
+                dataParsed.argValue1 = Convert.ToDouble(dataUnassigned[11]);
+                dataParsed.argPosition[1] = Convert.ToInt32(dataUnassigned[13]);
+                dataParsed.argName[1] = dataUnassigned[14];
+                dataParsed.argValue2 = dataUnassigned[16];
+            }
+            else
+            {
+                dataParsed.argPosition[1] = Convert.ToInt32(dataUnassigned[8]);
+                dataParsed.argName[1] = dataUnassigned[9];
+                dataParsed.argValue2 = dataUnassigned[11];
+                dataParsed.argPosition[0] = Convert.ToInt32(dataUnassigned[13]);
+                dataParsed.argName[0] = dataUnassigned[14];
+                dataParsed.argValue1 = Convert.ToInt32(dataUnassigned[16]);
+            }
 
-            
 
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new MemoryStream();
-            formatter.Serialize(stream, dataParsed);
-            return stream;
+            return dataParsed;
         }
 
         /*
